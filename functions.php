15a5a743178ab3d5e4c7f081f479d7d1efc674c1
Bozhidar Slaveykov <bobi@microweber.com>
2024-02-07 14:49:22 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,27 +2,32 @@
 
 event_bind('mw.front', function () {
 
-    $settings = get_option('settings', 'tawkto');
-    if($settings) {
-        $json = json_decode($settings, true);
+    $directChatLink = get_option('direct_chat_link', 'tawkto');
+    // check url is valid
+    if (filter_var($directChatLink, FILTER_VALIDATE_URL)) {
+
+        $parseDirectChatLink = str_replace("http://tawk.to/chat/","", $directChatLink);
+        $parseDirectChatLink = str_replace("https://tawk.to/chat/","", $parseDirectChatLink);
+        
+        if (!empty($parseDirectChatLink)) {
+            mw()->template->foot("
+
+                    <!--Start of Tawk.to Script-->
+                <script type=\"text/javascript\">
+                    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                    (function(){
+                        var s1=document.createElement(\"script\"),s0=document.getElementsByTagName(\"script\")[0];
+                        s1.async=true;
+                        s1.src='https://embed.tawk.to/" . $parseDirectChatLink . "';
+                        s1.charset='UTF-8';
+                        s1.setAttribute('crossorigin','*');
+                        s0.parentNode.insertBefore(s1,s0);
+                    })();
+                </script>
+                <!--End of Tawk.to Script-->
+
+                    ");
+        }
     }
-
-    mw()->template->foot("
-
-    <!--Start of Tawk.to Script-->
-<script type=\"text/javascript\">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement(\"script\"),s0=document.getElementsByTagName(\"script\")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/65a92ac60ff6374032c1c9de/1hkecu639';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
-<!--End of Tawk.to Script-->
-
-    ");
 
 });
